b4d72cd6d74f369cb3bca050501744f4796ee963
Maxim Lebedev <lmax942@gmail.com>
2021-08-19 21:34:01 +0300

    Custom Table View Cell

---
+++ BestPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  BestPlaces
+//
+//  Created by Максим Лебедев on 19.08.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
